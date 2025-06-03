5dcc975c9c7e8352963186b59b833c21aa5e52b6
Oscar To <Oscar To>
2025-06-03 15:08:00 +0700

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,12 @@
+async function getStats() {
+    const inputName = document.getElementById("username").value;
+    const inputTag = document.getElementById("tag").value;
+    const inputRegion = document.getElementById("region").value;
+    const output = document.getElementById("output");
+    const response = await fetch(`/api/stats?name=${inputName}&tag=${inputTag}&region=${inputRegion}`);
+    const data = await response.json();
+output.textContent = 
+  `Rank: ${data.rank}` +
+  `${data.rrCount}RR,` +
+  `Games Played: ${data.gamesPlayed}`;
+}
